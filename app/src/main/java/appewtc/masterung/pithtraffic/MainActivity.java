b893146d2acba2176d8134584f5d5e1556e2798f
masterUNG <phrombutr@gmail.com>
2015-11-19 15:56:23 +0700

    Day 1 Finish

--- app/src/main/java/appewtc/masterung/pithtraffic/MainActivity.java
+++ app/src/main/java/appewtc/masterung/pithtraffic/MainActivity.java
@@ -1,7 +1,11 @@
 package appewtc.masterung.pithtraffic;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -20,8 +24,31 @@ public class MainActivity extends AppCompatActivity {
         //Bind Widget
         bindWidget();
 
+        //Button Controller
+        buttonController();
+
 
     }   // Main Method
+
+    private void buttonController() {
+
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Sound Effect
+                MediaPlayer buttonMediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.cow);
+                buttonMediaPlayer.start();
+
+                //Intent to WebView
+                Intent objIntent = new Intent(Intent.ACTION_VIEW);
+                objIntent.setData(Uri.parse("https://youtu.be/AFmWqLIqDZA"));
+                startActivity(objIntent);
+
+            }   // event
+        });
+
+    }
 
     private void bindWidget() {
 
---
+++ app/src/main/java/appewtc/masterung/pithtraffic/MyAdapter.java
@@ -0,0 +1,10 @@
+package appewtc.masterung.pithtraffic;
+
+/**
+ * Created by masterUNG on 11/19/15 AD.
+ */
+public class MyAdapter {
+
+
+
+}   // Main Class
