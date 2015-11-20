72e09913c1dfe870b9f7e6363de3ef7a9ec8daa1
masterUNG <phrombutr@gmail.com>
2015-11-20 10:19:14 +0700

    Create ListView Successful

--- app/src/main/java/appewtc/masterung/pithtraffic/MyAdapter.java
+++ app/src/main/java/appewtc/masterung/pithtraffic/MyAdapter.java
@@ -1,10 +1,59 @@
 package appewtc.masterung.pithtraffic;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 /**
  * Created by masterUNG on 11/19/15 AD.
  */
-public class MyAdapter {
+public class MyAdapter extends BaseAdapter{
 
+    //Explicit
+    private Context objContext;
+    private int[] iconInts;
+    private String[] titleStrings;
 
+    public MyAdapter(Context objContext, int[] iconInts, String[] titleStrings) {
+        this.objContext = objContext;
+        this.iconInts = iconInts;
+        this.titleStrings = titleStrings;
+    }   // Constructor
 
+    @Override
+    public int getCount() {
+        return titleStrings.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+        //Open Service
+        LayoutInflater objLayoutInflater = (LayoutInflater) objContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View objView1 = objLayoutInflater.inflate(R.layout.traffic_listview, viewGroup, false);
+
+        //Setup Icon
+        ImageView iconImageView = (ImageView) objView1.findViewById(R.id.imvIcon);
+        iconImageView.setImageResource(iconInts[i]);
+
+        //Setup Title
+        TextView titleTextView = (TextView) objView1.findViewById(R.id.txtShowTitle);
+        titleTextView.setText(titleStrings[i]);
+
+        return objView1;
+    }
 }   // Main Class
